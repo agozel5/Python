51f945f010fa915361cbbef3d865c090ab2d808d
agozel5 <118749892+agozel5@users.noreply.github.com>
2023-01-12 14:59:23 +0100

    Update Notes.py

--- TP5/Notes.py
+++ TP5/Notes.py
@@ -4,7 +4,7 @@ for i in range(5):
     s = input("Veuillez entrer la note du module " + str(i+1) + " et le coefficient correspondant : ")
     s = s.split(" ")
     notes.append(float(s[0]))
-    coefficients.append(int(s[1]))
+    coefficients.append(float(s[1]))
 
 total_coefficients = sum(coefficients)
 total_notes = 0
