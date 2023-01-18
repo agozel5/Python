4331063b3480062461965bfe0f9250f5af0078fb
agozel5 <118749892+agozel5@users.noreply.github.com>
2023-01-18 10:15:25 +0100

    Update Exercice3.py

--- TP6/Exercice3.py
+++ TP6/Exercice3.py
@@ -2,22 +2,16 @@ def ajouter_elt(lst=[0,1,2], elt=3):
     lst.append(elt)
     return lst
 
-#a)
-print(ajouter_elt()) #affiche [0,1,2,3]
+print(ajouter_elt())
 
-#b)
-print(ajouter_elt()) #affiche [0,1,2,3]
-print(id(ajouter_elt())) #affiche l'id de la liste retournée par la fonction
-# La fonction retourne la même liste avec les mêmes valeurs pour les deux appels car c'est la même instance de la liste qui est utilisée en mémoire
+print(ajouter_elt()) 
+print(id(ajouter_elt())) 
 
-#c)
 def ajouter_carac(ch='abc', elt='d'):
     return ch + elt
 
-#d)
-print(ajouter_carac()) #affiche 'abcd'
+print(ajouter_carac()) 
 
-#e)
-print(ajouter_carac()) #affiche 'abcd'
-print(id(ajouter_carac())) #affiche l'id de la chaine retournée par la fonction
-# La fonction retourne la même chaine avec les mêmes valeurs pour les deux appels car c'est la même instance de la chaine qui est utilisée en mémoire
+print(ajouter_carac()) 
+print(id(ajouter_carac())) 
+
