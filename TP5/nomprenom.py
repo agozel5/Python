5d02f4a9bb1970649ffc0302de8a57306a0f6109
agozel5 <118749892+agozel5@users.noreply.github.com>
2023-01-18 10:21:57 +0100

    Update nomprenom.py

--- TP5/nomprenom.py
+++ TP5/nomprenom.py
@@ -1,8 +1,8 @@
-# Nom/ Prénom 1er personne
+# Demande d'entrée le nom de la première personne
 nom1 = input("Entrez le nom de la première personne : ")
 prenom1 = input("Entrez le prénom de la première personne : ")
 
-# Nom/ Prénom 2ème personne
+# Nom de la deuxième personne
 nom2 = input("Entrez le nom de la deuxième personne : ")
 prenom2 = input("Entrez le prénom de la deuxième personne : ")
 
@@ -12,14 +12,14 @@ prenom1 = prenom1.capitalize()
 nom2 = nom2.upper()
 prenom2 = prenom2.capitalize()
 
-# On compare les noms et on affiche les prénoms et les noms dans l'ordre alphabétique
+# Compare
 if nom1 < nom2:
   print(prenom1, nom1)
   print(prenom2, nom2)
 elif nom1 > nom2:
   print(prenom2, nom2)
   print(prenom1, nom1)
-else: # Si les noms sont égaux, on compare les prénoms
+else: # compare prénom si nom sont égaux
   if prenom1 < prenom2:
     print(prenom1, nom1)
     print(prenom2, nom2)
